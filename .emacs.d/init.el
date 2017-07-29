87edb90b73cbe13d666f06b25e9a217f5e89b12f
Alex Pate <proto.merkabah@gmail.com>
2017-07-29 13:56:56 -0400

    Ensure packages get installed for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,10 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; Package Repos
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
@@ -26,8 +26,6 @@
 (setq linum-format "%d ")
 
 
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -37,26 +35,35 @@
    (quote
     ("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
  '(package-selected-packages (quote (flycheck color-theme-sanityinc-tomorrow js2-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
+
+
+;; Ensure packages get installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 
 ;; Flycheck - Syntax Linting
-(require 'flycheck)
-(add-hook 'after-init-hook 'global-flycheck-mode)
+(use-package flycheck
+  :init
+  (add-hook 'after-init-hook 'global-flycheck-mode))
 
 
 ;; JavScript
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-hook 'js2-mode-hook
-	  (lambda ()
-	    (setq js2-basic-offset 2)
-	    (setq js2-basic-indent 2)
-	    (setq js2-auto-indent-p t)))
+(use-package js2-mode
+  :mode ("\\.js\\'" . js2-mode)
+  :config
+  (progn
+    (add-hook 'js2-mode-hook
+	      (lambda ()
+		(setq js2-basic-offset 2)
+		(setq js2-basic-indent 2)
+		(setq js2-auto-indent-p t)))))
 
-(require 'color-theme-sanityinc-tomorrow)
-(load-theme 'sanityinc-tomorrow-blue)
+(use-package color-theme-sanityinc-tomorrow
+  :init
+  (load-theme 'sanityinc-tomorrow-blue :no-confirm))
